db738bbf73a16b2e161b4fa09becd9cb3d48e7b2
AnkithSanga <ankithsanga1234@gmail.com>
2022-11-22 19:22:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const articleRouter = require('./routes/articles')
+const app = express()
+const methodOverride = require('method-override')
+
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/blog')
+app.set('view engine','ejs')
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended:false}))
+app.get('/',async (req,res)=> {
+    const articles = await Article.find().sort({createdAt:'desc'})
+    res.render('articles/index',{articles:articles})
+})
+app.use('/articles',articleRouter)
+app.listen(5000)
